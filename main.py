a542409db70ed48a4a7cf9c7d3a0f165cafb57a4
h0nus <ivanbiagi7@protonmail.com>
2019-05-02 00:29:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 *-*
-from PIL import Image
 import qrcode
 import subprocess
+import sys
+import argparse
+from PIL import Image
+qr_version="0.1"
+banner='''
+  e88 88e   888 88e    e88'Y88
+ d888 888b  888 888D  d888  'Y   ,e e,  888 8e
+C8888 8888D 888 88"  C8888 eeee d88 88b 888 88b
+ Y888 888P  888 b,    Y888 888P 888   , 888 888
+  "88 88"   888 88b,   "88 88"   "YeeP" 888 888
+      b
+      8b,    {}'''.format("QRGen ~ v0.1 ~ by h0nus\n")
+print(banner)
+#print('Tool to generate Malformed QRCodes for fuzzing QRCode parsers/reader\n')
+parser = argparse.ArgumentParser(description="Tool to generate Malformed QRCodes for fuzzing QRCode parsers/reader",
+                                 usage='qrgen.py -l [number]\nusage: qrgen.py -w [/path/to/custom/wordlist]',
+                                 epilog="Pay attention everywhere, even in the dumbest spot")
+sgroup = parser.add_argument_group("Options for QRGen")
+sgroup.add_argument("--list","-l",type=int,help="Set wordlist to use",choices=[0,1,2,3,4,5,6,7])
+sgroup.add_argument("--wordlist","-w",type=str,default=None,help="Use a custom wordlist")
+
+options = parser.parse_args()
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(1)
+
+lists = ['sqli','xss','cmdinj','formatstr','xxe','strfuzz','ssi','lfi-dt']
 try:
     cmd= subprocess.check_output(['mkdir','genqr'],stderr=subprocess.STDOUT)
     print("Payload path generated..")
@@ -15,17 +42,19 @@ try:
 except:
     print("Path already cleared or deleted..")
     pass
-payloads = open("payloads.txt").readlines()
-i = 0
-while i < len(payloads):
-    payloads[i] = payloads[i].strip()
-    i+=1
+payloads = []
+if options.list:
+    z = options.list
+    payloads = open(lists[z]).readlines()
+elif options.wordlist:
+    z = options.wordlist
+    payloads = open(z).readlines()
 i = 0
 while i < len(payloads):
     img = qrcode.make(payloads[i])
     img.save("genqr/payload-{}.png".format(i))
     i+=1
 print("Generated {} payloads!".format(len(payloads)))
-print("Opening last generated payload...")
 Image.open("genqr/payload-{}.png".format(i-1)).show()
+print("Opening last generated payload...")
 print("Thanks for using QRGen, made by H0nus..")
